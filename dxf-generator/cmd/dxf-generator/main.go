9042beb5154449e02c749b1980740a2191f907c2
Mark Saravi <mark.saravi@gmail.com>
2022-10-30 19:23:39 +1100

    drawing

--- dxf-generator/cmd/dxf-generator/main.go
+++ dxf-generator/cmd/dxf-generator/main.go
@@ -9,37 +9,56 @@ import (
 	"github.com/marksaravi/dxfgenerator/epitrochoid"
 )
 
+const svg = `
+<svg viewBox="%f %f %f %f" xmlns="http://www.w3.org/2000/svg">
+ <style>svg { background-color: white; }</style>
+ %s
+</svg>
+`
+
 func main() {
 	fmt.Println("DXF Generator")
 	const scale float64 = 100
-	const da = math.Pi / (180 * 5)
+	const endAngle float64 = math.Pi
+	const chunkSize = 180
+	const numChunks = 10
+
+	const da = endAngle / (chunkSize * numChunks)
 	const R = float64(3) * scale
 	const r = float64(1) * scale
 	const d = float64(2) * scale
-	builder := strings.Builder{}
-	var min float64 = 0
-	var max float64 = 0
-	var offset float64 = 1000
-	for angle := float64(0); angle < 2*math.Pi; angle += da {
-		x, y := epitrochoid.Hypocycloid(angle, R, r, d)
-		x += offset
-		y += offset
-		builder.WriteString(fmt.Sprintf(",%f,%f", x, y))
-		if x > max {
-			max = x
+	var minx, miny float64 = 1000000, 1000000
+	var maxx, maxy float64 = -1000000, -1000000
+	polylines := strings.Builder{}
+
+	for i := 0; i < numChunks; i++ {
+		builder := strings.Builder{}
+		comma := ""
+		builder.WriteString("<polyline  stroke-width=\"1\" fill=\"none\" stroke=\"black\" points=\"")
+		for j := 0; j < chunkSize; j++ {
+			angle := float64(i*chunkSize+j) * da
+			x, y := epitrochoid.Hypocycloid(angle, R, r, d)
+			builder.WriteString(fmt.Sprintf("%s%f,%f", comma, x, y))
+			comma = ","
+			if x > maxx {
+				maxx = x
+			}
+			if y > maxy {
+				maxy = y
+			}
+			if x < minx {
+				minx = x
+			}
+			if y < miny {
+				miny = y
+			}
 		}
-		if y > max {
-			max = y
-		}
-		if x < min {
-			min = x
-		}
-		if y < min {
-			min = y
-		}
+		builder.WriteString("\" />")
+		polylines.WriteString(builder.String())
 	}
-	fmt.Println(min, max)
-	fout, _ := os.Create("./data")
-	fout.WriteString(builder.String())
+
+	fmt.Println(minx, miny, maxx, maxy)
+	fout, _ := os.Create("./rotor.svg")
+	fout.WriteString(fmt.Sprintf(svg, minx-50, miny-50, maxx+50, maxy+50, polylines.String()))
 	fout.Close()
 }
